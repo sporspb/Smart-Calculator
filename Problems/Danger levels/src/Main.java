3db36cc8a18de3eb6d3c7caff34f7503c0cd3f62
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2020-12-07 15:58:16 +0300

    completed Fields and methods in enum

---
+++ Problems/Danger levels/src/Main.java
@@ -0,0 +1,15 @@
+enum DangerLevel {
+    HIGH(3),
+    MEDIUM(2),
+    LOW(1);
+
+    int num;
+
+    DangerLevel(int num) {
+        this.num = num;
+    }
+
+    public int getLevel() {
+        return this.num;
+    }
+}
