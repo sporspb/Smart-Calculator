ad4185175a872dae5a129469428f6a394ebe4ce6
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2020-12-08 16:34:19 +0300

    completed Manipulating fields and methods

---
+++ Problems/Field value/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Get value for a given public field or null if the field does not exist or is not accessible.
+ */
+class FieldGetter {
+
+    public Object getFieldValue(Object object, String fieldName) throws IllegalAccessException {
+        try {
+            return object.getClass().getField(fieldName).get(object);
+        } catch (Exception ignored) {
+            return null;
+        }
+    }
+
+}
